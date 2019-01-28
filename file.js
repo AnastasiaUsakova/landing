2a085fad1ddb0c09d13e078190dcec5dc73cca6e
Your Name <you@example.com>
2019-01-28 12:02:37 +0300

    edited style

---
+++ file.js
@@ -0,0 +1,6 @@
+function send() {
+    document.getElementById('text1').value='';
+    document.getElementById('text2').value='';
+    document.getElementById('text3').value='';
+    document.getElementById('text4').value='';
+}
